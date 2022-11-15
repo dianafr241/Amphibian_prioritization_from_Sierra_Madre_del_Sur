23bd3d1144d881d5ecb9fdf993e8b530fe5fd98e
Diana Laura Fuentes de la Rosa <72981716+dianafr241@users.noreply.github.com>
2022-11-15 00:02:24 -0600

    Settings

---
+++ Settings/C-CAZ.bat
@@ -0,0 +1,1 @@
+call zig3.exe -r IUCN_NOM_3_escenarios_CAZ/Standar_groups_C-CAZ.dat IUCN_NOM_3_escenarios_CAZ/IUCN_NOM_3_escenarios.spp basic_output/IUCN_NOM_3_escenarios_CAZ.txt 0.0 0 0 0
---
+++ Settings/C_ABF.bat
@@ -0,0 +1,1 @@
+call zig3.exe -r IUCN_NOM_3_escenarios_ABF/Standar_groups_C-ABF.dat IUCN_NOM_3_escenarios_ABF/IUCN_NOM_3_escenarios.spp basic_output/IUCN_NOM_3_escenarios_ABF.txt 0.0 0 0 0
